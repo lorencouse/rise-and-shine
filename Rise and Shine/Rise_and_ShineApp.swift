852f1d7949d7c4d1a85162394e70d8453b83b021
Loren Couse <couselm@gmail.com>
2024-01-13 07:12:45 +0800

    Initial Commit

---
+++ Rise and Shine/Rise_and_ShineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rise_and_ShineApp.swift
+//  Rise and Shine
+//
+//  Created by loren on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Rise_and_ShineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
